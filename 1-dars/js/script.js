fa7a2da71f99771e04776ec6dfac430ef3ccb294
shaxa_9505 <87860218+shaxa9505@users.noreply.github.com>
2023-01-05 10:41:18 +0500

    Add files via upload

---
+++ 1-dars/js/script.js
@@ -0,0 +1,23 @@
+let number = 10;
+const borderColor = "red";
+console.log(number);
+
+number = "Salom Shohrux";
+console.log(number);
+
+const myName = "Shohrux";
+
+// borderColor = "blue";
+// console.log(borderColor);
+
+const person = {
+  name: "Samar"
+}
+
+person.name = "Yusuf";
+console.log(person.name);
+
+// Old versions 
+
+console.log(clientName);
+var clientName = "Yusuf";
